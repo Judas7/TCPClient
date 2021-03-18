b3aa87f390dbfc5206214932504fb99197f31761
Linus Landin <linus.landin@edu.lycksele.se>
2021-03-18 10:53:40 +0100

    Add project files.

---
+++ TCPClient/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TCPClient
+{
+    partial class TCPClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.tbxIPAdress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSayHello
+            // 
+            this.btnSayHello.Location = new System.Drawing.Point(80, 238);
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.Size = new System.Drawing.Size(143, 44);
+            this.btnSayHello.TabIndex = 0;
+            this.btnSayHello.Text = "Say Hello";
+            this.btnSayHello.UseVisualStyleBackColor = true;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // tbxIPAdress
+            // 
+            this.tbxIPAdress.Location = new System.Drawing.Point(80, 198);
+            this.tbxIPAdress.Name = "tbxIPAdress";
+            this.tbxIPAdress.Size = new System.Drawing.Size(143, 26);
+            this.tbxIPAdress.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Servers IP-Adress";
+            // 
+            // TCPClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 331);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxIPAdress);
+            this.Controls.Add(this.btnSayHello);
+            this.Name = "TCPClient";
+            this.Text = "TCP Client";
+            this.Load += new System.EventHandler(this.TCPClient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSayHello;
+        private System.Windows.Forms.TextBox tbxIPAdress;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TCPClient/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPClient
+{
+    public partial class TCPClient : Form
+    {
+
+        TcpClient client;
+        int port = 12345;
+
+        public TCPClient()
+        {
+            InitializeComponent();
+        }
+
+        private void TCPClient_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayHello_Click(object sender, EventArgs e)
+        {
+            IPAddress adress = IPAddress.Parse(tbxIPAdress.Text);
+            client = new TcpClient();
+            client.NoDelay = true;
+            client.Connect(adress, port);
+
+            if (client.Connected)
+            {
+                byte[] outData = Encoding.Unicode.GetBytes("Hello!");
+                client.GetStream().Write(outData, 0, outData.Length);
+                client.Close();
+            }
+        }
+    }
+}
